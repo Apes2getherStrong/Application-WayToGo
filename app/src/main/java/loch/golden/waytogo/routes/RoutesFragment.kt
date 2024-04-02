2a1bb9b18ad8f5d444b0a0b9f9ee37a6cebafaf8
turboblaster44 <133587818+turboblaster44@users.noreply.github.com>
2024-04-02 18:11:25 +0200

    myRoutes tab with ViewPager

--- app/src/main/java/loch/golden/waytogo/routes/RoutesFragment.kt
+++ app/src/main/java/loch/golden/waytogo/routes/RoutesFragment.kt
@@ -4,33 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.paging.filter
-import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 import loch.golden.waytogo.databinding.FragmentRoutesBinding
-import loch.golden.waytogo.map.MapViewModel
-import loch.golden.waytogo.routes.adapter.RecyclerViewRouteAdapter
-import loch.golden.waytogo.routes.repository.RouteRepository
-import loch.golden.waytogo.routes.viewmodel.RouteViewModel
-import loch.golden.waytogo.routes.viewmodel.RouteViewModelFactory
+import loch.golden.waytogo.routes.adapter.RoutesViewPagerAdapter
 
-class RoutesFragment : Fragment() {
+class RoutesFragment : Fragment(), OnTabSelectedListener {
 
     private lateinit var binding: FragmentRoutesBinding
-    private lateinit var recyclerViewRouteAdapter: RecyclerViewRouteAdapter
-    private val mapViewModel by activityViewModels<MapViewModel>()
-    private lateinit var routeViewModel: RouteViewModel
-    private lateinit var searchView: SearchView
-    private val viewModel by viewModels<RouteViewModel>()
+    private lateinit var routesViewPagerAdapter: RoutesViewPagerAdapter
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         binding = FragmentRoutesBinding.inflate(inflater, container, false)
         return binding.root
@@ -38,62 +27,23 @@ class RoutesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initSearchView()
-        initRecyclerView()
-        initViewModel()
-        //binding.recyclerViewRoutes.addOnScrollListener(Rec)
-
-    }
-
-    private fun initRecyclerView() {
-        recyclerViewRouteAdapter = RecyclerViewRouteAdapter()
-        binding.recyclerViewRoutes.adapter = recyclerViewRouteAdapter
-        binding.recyclerViewRoutes.layoutManager = LinearLayoutManager(requireContext())
-    }
-
-    private fun initViewModel() {
-        val repository = RouteRepository()
-        val routeViewModelFactory = RouteViewModelFactory(repository)
-        routeViewModel = ViewModelProvider(this, routeViewModelFactory)[RouteViewModel::class.java]
-        observeRouteResponse()
-    }
-
-    private fun observeRouteResponse() {
-
-        lifecycleScope.launch {
-            viewModel.getRoutes(0, 20).collectLatest { pagingData ->
-                recyclerViewRouteAdapter.submitData(pagingData)
-            }
-        }
-    }
-
-    private fun initSearchView() {
-
-        searchView = binding.searchView
-
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                search(newText)
-                return true
+        routesViewPagerAdapter = RoutesViewPagerAdapter(this)
+        binding.viewPager.adapter = routesViewPagerAdapter
+        binding.tabLayout.addOnTabSelectedListener(this)
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.tabLayout.getTabAt(position)?.select()
             }
         })
     }
 
-    private fun search(query: String?) {
-        query?.let { searchedQuery ->
-            lifecycleScope.launch {
-                viewModel.getRoutes(0, 20).collectLatest { pagingData ->
-                    val filteredRoutes = pagingData.filter { route ->
-                        route.name.contains(searchedQuery, ignoreCase = true)
-                    }
-                    recyclerViewRouteAdapter.submitData(filteredRoutes)
-                }
-            }
-        }
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        binding.viewPager.currentItem = tab!!.position
     }
-}
 
+    override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+    override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+}
