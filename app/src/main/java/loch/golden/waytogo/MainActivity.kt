c6e767895cee7bc4be7e1354ecdc427142dbcef6
turboblaster44 <133587818+turboblaster44@users.noreply.github.com>
2024-04-04 17:03:07 +0200

    Make location mandatory

--- app/src/main/java/loch/golden/waytogo/MainActivity.kt
+++ app/src/main/java/loch/golden/waytogo/MainActivity.kt
@@ -1,15 +1,18 @@
 package loch.golden.waytogo
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatCallback
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.commit
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.navigation.NavigationBarView
 import loch.golden.waytogo.databinding.ActivityMainBinding
 import loch.golden.waytogo.map.PointMapFragment
@@ -20,28 +23,54 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
     ActivityCompat.OnRequestPermissionsResultCallback {
 
     private lateinit var binding: ActivityMainBinding
-
+    private var dialog: AlertDialog? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        handlePermissions()
-        binding.bottomNav.setOnItemSelectedListener(this)
     }
 
-    private fun handlePermissions() {
+    override fun onStart() {
+        super.onStart()
+        handlePermissions(true)
+    }
+
+    private fun handlePermissions(requestPerm: Boolean) {
+        dialog?.dismiss()
         if (Permissions.isPermissionGranted(this, Manifest.permission.ACCESS_FINE_LOCATION))
-            addMap()
+            setUpView()
         else {
-            Permissions.requestPermission(
-                this,
-                arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ),
-                Permissions.LOCATION_PERMISSION_REQUEST_CODE
-            )
+            if (requestPerm) {
+                Permissions.requestPermission(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    Permissions.LOCATION_PERMISSION_REQUEST_CODE
+                )
+            } else {
+                dialog = MaterialAlertDialogBuilder(this).apply {
+                    setTitle("Requesting Permissions")
+                    setMessage("You need to allow WayToGo to access this device's location")
+                    setPositiveButton("Retry") { dialog, _ ->
+                        handlePermissions(false)
+                        dialog.dismiss()
+                    }
+                    setNeutralButton("More info") { _, _ ->
+                        intent = Intent(Intent.ACTION_VIEW)
+                        intent.data =
+                            Uri.parse("https://support.google.com/android/answer/9431959?hl=en")
+                        startActivity(intent)
+
+                    }
+                    setOnDismissListener() {
+                        Log.d("Warmbier", "Im being dismissed :(")
+                    }
+                }.create()
+                dialog?.show()
+            }
+
         }
     }
 
@@ -55,17 +84,13 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         if (requestCode == Permissions.LOCATION_PERMISSION_REQUEST_CODE) {
             // Check if the user granted the location permissions
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, proceed with map setup
-                Toast.makeText(this, "Location permission good", Toast.LENGTH_SHORT).show()
+                setUpView()
 
             } else {
-                // Permission denied, handle accordingly
-                Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show()
+                handlePermissions(false)
             }
         }
     }
-
-
 
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -85,10 +110,11 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
     }
 
 
-    private fun addMap() {
+    private fun setUpView() {
         supportFragmentManager.commit {
             replace(R.id.fragment_container_main, PointMapFragment())
         }
+        binding.bottomNav.setOnItemSelectedListener(this)
     }
 
     override fun onBackPressed() {
@@ -99,5 +125,6 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
             super.onBackPressed()
         }
     }
-//TODO on permission decline make the screen tell u need to enable it
+
+
 }
