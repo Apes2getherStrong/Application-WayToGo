acb05227bcaac8ed9c782138f56d494cfec480ec
DasGinta <gogaczkacper123@gmail.com>
2024-05-04 12:00:59 +0200

    MyRoutes fragment implementation

--- app/src/main/java/loch/golden/waytogo/routes/MyRoutesFragment.kt
+++ app/src/main/java/loch/golden/waytogo/routes/MyRoutesFragment.kt
@@ -6,15 +6,27 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.filter
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import loch.golden.waytogo.databinding.FragmentMyRoutesBinding
 import loch.golden.waytogo.databinding.FragmentPublicRoutesBinding
 import loch.golden.waytogo.map.creation.CreationPrefManager
+import loch.golden.waytogo.routes.adapter.RecyclerViewRouteAdapter
+import loch.golden.waytogo.routes.model.Route
+import loch.golden.waytogo.routes.viewmodel.RouteViewModel
 import java.io.File
 
 class MyRoutesFragment : Fragment() {
 
     private lateinit var binding: FragmentMyRoutesBinding
+    private lateinit var routeViewModel: RouteViewModel
+    private lateinit var recyclerViewRouteAdapter: RecyclerViewRouteAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,6 +40,52 @@ class MyRoutesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 //        listFromFolder()
         listFromPref()
+        initSearchView()
+        initRecyclerView()
+    }
+    private fun initRecyclerView() {
+        recyclerViewRouteAdapter = RecyclerViewRouteAdapter()
+        binding.recyclerViewRoutes.adapter = recyclerViewRouteAdapter
+        binding.recyclerViewRoutes.layoutManager = LinearLayoutManager(requireContext())
+
+        recyclerViewRouteAdapter.setOnClickListener(object : RecyclerViewRouteAdapter.OnClickListener {
+            override fun onItemClick(position: Int, route: Route) {
+                val id = route.routeUid
+                val bundle = Bundle().apply {
+                    putString("id", id)
+                }
+                val fr = RouteDetailFragment()
+                fr.arguments = bundle // Set the arguments bundle to the fragment
+                (parentFragment as? RoutesFragment)?.replaceFragment(1,fr)
+            }
+        })
+
+    }
+
+    private fun initSearchView() {
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                search(newText)
+                return true
+            }
+        })
+    }
+    private fun search(query: String?) {
+        query?.let { searchedQuery ->
+            lifecycleScope.launch {
+                routeViewModel.getRoutes(0, 20).collectLatest { pagingData ->
+                    val filteredRoutes = pagingData.filter { route ->
+                        route.name.contains(searchedQuery, ignoreCase = true)
+                    }
+                    recyclerViewRouteAdapter.submitData(filteredRoutes)
+                }
+            }
+        }
     }
 
 
