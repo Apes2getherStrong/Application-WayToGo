35c8d365cb7498ef8a8b0b97a0559ba06b204144
DasGinta <gogaczkacper123@gmail.com>
2024-10-13 11:49:48 +0200

    Added token management and get/save username methods

--- app/src/main/java/loch/golden/waytogo/user/tokenmanager/TokenManager.kt
+++ app/src/main/java/loch/golden/waytogo/user/tokenmanager/TokenManager.kt
@@ -2,6 +2,8 @@ package loch.golden.waytogo.user.tokenmanager
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.auth0.jwt.JWT
+import com.auth0.jwt.interfaces.DecodedJWT
 
 class TokenManager (context: Context) {
 
@@ -16,5 +18,45 @@ class TokenManager (context: Context) {
 
     fun getToken() : String? {
         return prefs.getString("auth_token",null);
+    }
+
+    fun clearToken() {
+        with(prefs.edit()) {
+            remove("auth_token")
+            apply()
+        }
+    }
+
+   fun isTokenExpired(token: String): Boolean {
+        val decodedJWT: DecodedJWT = JWT.decode(token)
+        val expiresAtMillis = decodedJWT.expiresAt?.time ?: return true
+        val currentTimeMillis = System.currentTimeMillis()
+        return expiresAtMillis < currentTimeMillis
+   }
+
+    fun getUserFromJWT(): String? {
+        val token = getToken()
+        return if (token != null) {
+            try {
+                val decodedJWT: DecodedJWT = JWT.decode(token)
+                decodedJWT.getClaim("username").asString()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
+        } else {
+            null
+        }
+    }
+
+    fun saveUsername(username: String) {
+        with(prefs.edit()) {
+            putString("username", username)
+            apply()
+        }
+    }
+
+    fun getUsername(): String? {
+        return prefs.getString("username", null)
     }
 }
